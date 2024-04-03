393b86214c700bbbf42bdcd33c8a4629afa3dd63
ohyo555 <555gywn@gamil.com>
2024-04-03 11:18:20 +0900

    todo 리스팅

--- src/app/theme.js
+++ src/app/theme.js
@@ -2,12 +2,15 @@ import { createTheme } from '@mui/material';
 
 const theme = createTheme({
   palette: {
+    typocraphy: {
+      fontFamily: ['GmarketSansMedium'],
+    },
     type: 'dark',
     primary: {
-      main: '#4caf50',
+      main: '#d8e49f',
     },
     secondary: {
-      main: '#4caf50',
+      main: '#d8e49f',
     },
   },
 });
